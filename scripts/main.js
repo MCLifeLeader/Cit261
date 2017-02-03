0151b421e94b34ce7cf897a823462f87955a43e4
Michael Carey <kb7ppb@hotmail.com>
2017-02-02 19:44:26 -0700

    CIT 261 Prov project space. Primarily adding DOM and XMLHTTPRequest examples

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,8 @@
 */
 
 // http://www.w3schools.com/js/js_cookies.asp
-function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays) 
+{
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
@@ -12,44 +13,50 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 // http://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
+function getCookie(cname) 
+{
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+
+    for(var i = 0; i <ca.length; i++) 
+    {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) == ' ') 
+        {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) == 0) 
+        {
             return c.substring(name.length, c.length);
         }
     }
     return "";
 }
 
-function redirectUrl(curl)
+function redirectUrl(curl) 
 {
 	window.location = curl;
 }
 
 // http://stackoverflow.com/questions/14446447/javascript-read-local-text-file
-function readTextFile(file)
+function readTextContent(uncPath) 
 {
-	var allText = "";
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
+    rawFile.open("GET", uncPath, true);
+
+    rawFile.onreadystatechange = function () 
     {
-        if(rawFile.readyState === 4)
+        if(rawFile.readyState === 4 || rawFile.readyState === XMLHttpRequest.DONE)
         {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
-                allText = rawFile.responseText;
+                return rawFile.responseText;
             }
         }
     }
-    rawFile.send(null);
-	
-	return allText;
+
+    rawFile.send();
+
+	return null;
 }
