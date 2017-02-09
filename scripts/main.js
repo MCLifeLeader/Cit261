de732a273672a9cef30e8bed4cbdfb7492941f63
Michael Carey <kb7ppb@hotmail.com>
2017-02-08 23:23:54 -0700

    Fixed AJAX Load issue.

--- scripts/main.js
+++ scripts/main.js
@@ -60,3 +60,16 @@ function readTextContent(uncPath)
 
 	return null;
 }
+
+//http://stackoverflow.com/questions/3387427/remove-element-by-id
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+//http://stackoverflow.com/questions/3387427/remove-element-by-id
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
